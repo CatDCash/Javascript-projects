506c3f4ed04b4dcd223cd8a1778a1da6547ae8d7
CatDCash <catdcash@proton.me>
2024-02-18 05:56:41 -0600

    new interface forrisk management

--- riskmanagement/app/app.js
+++ riskmanagement/app/app.js
@@ -3,29 +3,69 @@ window.onload = function() {
     loadExcelFile();
   };
 
+  //cosa qeu hace qeu la lista este mamalona
+  let listElements = document.querySelectorAll('.list__button--click');
+  listElements.forEach(listElement => {
+    listElement.addEventListener('click', () => {
+      listElement.classList.toggle('arrow');
+      
+      let height = 0
+      let menu = listElement.nextElementSibling
+      console.log(menu.scrollHeight)
+      if(menu.clientHeight == '0'){
+        height=menu.scrollHeight
+      }
+
+      menu.style.height = `${height}px`
+    })
+  });
+
+  // cosa para seleccionar el escenario :( y ver si puedo cerrar el menu
+  let listEscenarios = document.querySelectorAll('.nav__link--inside')
+  listEscenarios.forEach(escenario => {
+    escenario.addEventListener('click', () => {
+      selectedValue = escenario.textContent
+
+    })
+  })
+
+  const showMenu = (toggleId,navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show')
+        })
+    }
+  }
+
+showMenu('nav-toggle','nav-menu')
+
     // INJECT CSS
   var css = document.createElement('style');
   css.type = 'text/css';
   css.innerHTML = '#txt-rotate > .wrap { border-right: 0.08em solid #666 }';
   document.body.appendChild(css);
-
- /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+  
+  //variables para animacion del texto
   var element = document.getElementById('txt-rotate');
   var toRotate = element.getAttribute('data-rotate');
   var period = element.getAttribute('data-period');
   setTimeout(function() {
     new TxtRotate(element, JSON.parse(toRotate), period);
   }, 1500);
-  
+
+  // evento boton
   document.getElementById("buscar").addEventListener("click", function() {
+    console.log('click')
+    console.log(selectedValue)
     compareExcel();
+    //document.getElementsByClassName('list__show').height = '0'
   });
-  
+
   function loadExcelFile() {
-    var file = 'riesgos_test.xlsx'; // archivo donde se almacenan los datos de riesgos
+    var file = 'assets/riesgos_test.xlsx'; // archivo donde se almacenan los datos de riesgos
     var reader = new FileReader(); //objeto para leer el archivo
   
     reader.onload = function(e) {
@@ -42,17 +82,14 @@ window.onload = function() {
   }
   
   function compareExcel() {
-    var selectedValue = document.getElementById("escenario").value;
-    
+    //var selectedValue = document.getElementById("escenario").value;
     var workbook = window.workbook;
-  
     var sheetName = workbook.SheetNames[0];
     var worksheet = workbook.Sheets[sheetName];
-  
     var excelData = XLSX.utils.sheet_to_json(worksheet);
   
     var filteredData = excelData.filter(function(row) {
-      return row.riesgo === selectedValue;
+      return row.control === selectedValue;
     });
   
     var tablaHtml = '<tr><th>Control</th><th>Condicion</th><th>Descripcion</th><th>Mitigacion</th></tr>'; // 
